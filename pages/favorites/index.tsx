8313f6fb7a62a63a2732b48ffea55d5dc488b6c7
Alberto Chávez [GDL] <dchavez@compusoluciones.com>
2022-04-21 08:44:11 -0500

    Se añade página de favoritos

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,11 +1,29 @@
+import { Card, Grid } from "@nextui-org/react";
 import { NextPage } from "next";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MainLayout } from "../../components/layouts";
+import { FavoritePokemons } from "../../components/pokemon";
+import { NoFavorites } from "../../components/ui";
+import { localFavorites } from "../../utils";
 
 const FavoritesPage: NextPage = () => {
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons());
+  }, []);
+
+  console.log(
+    "🚀 ~ file: index.tsx ~ line 9 ~ favoritePokemons",
+    favoritePokemons
+  );
   return (
     <MainLayout title="Pokemons favoritos">
-      <h1>Pokemons favoritos</h1>
+      {favoritePokemons.length === 0 ? (
+        <NoFavorites />
+      ) : (
+        <FavoritePokemons pokemons={favoritePokemons} />
+      )}
     </MainLayout>
   );
 };
