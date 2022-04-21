e05c4d1c8b9f34dfc5d62c35538849168e572bba
Alberto Chávez [GDL] <dchavez@compusoluciones.com>
2022-04-21 08:45:05 -0500

    Se añade componente FavoritePokemons

---
+++ components/pokemon/FavoritePokemons.tsx
@@ -0,0 +1,16 @@
+import { Card, Grid } from "@nextui-org/react";
+import React, { FC } from "react";
+import FavoriteCardPokemon from "./FavoriteCardPokemon";
+
+interface Props {
+  pokemons: number[];
+}
+export const FavoritePokemons: FC<Props> = ({ pokemons }) => {
+  return (
+    <Grid.Container gap={2} direction="row" justify="flex-start">
+      {pokemons.map((pokemonId) => (
+        <FavoriteCardPokemon key={pokemonId} pokemonId={pokemonId} />
+      ))}
+    </Grid.Container>
+  );
+};
